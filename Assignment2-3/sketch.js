096d3c621711a6aa8eb5d1754eb2b707a28cb750
egioe2 <egioe2@lsu.edu>
2024-03-20 23:34:58 -0500

    2-3

---
+++ Assignment2-3/sketch.js
@@ -0,0 +1,32 @@
+let noise = new Tone.Noise("white");
+let filter = new Tone.Filter(5,"lowpass");
+
+noise.connect(filter);
+filter.toDestination();
+
+function preload()
+{
+  cnv = loadImage('assets/BlockBreak.png')
+}
+
+function setup() 
+{
+  createCanvas(400, 400);
+}
+
+function draw() 
+{
+  if(mouseIsPressed === true)
+  {
+    background(cnv);
+    noise.start();
+    filter.frequency.rampTo(0,2);
+  } 
+  else if(mouseIsPressed === false)
+  {
+    background(100);
+    text('Click and hold mouse', 150, 150);
+    noise.stop();
+    filter.frequency.value = 1000;
+  }
+}
