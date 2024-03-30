ed636671c3240da4f3dd0034ff80d2f3329131f3
egioe2 <egioe2@lsu.edu>
2024-03-29 21:48:20 -0500

    c

--- SoundProject/sketch.js
+++ SoundProject/sketch.js
@@ -3,7 +3,6 @@ let bugs = [];
 let score = 1;
 let timeRemaining = 30;
 let gameOver = false;
-let startScreen = true;
 
 let bgm = new Tone.Player("assets/bgm.mp3").toDestination();
 bgm.loop = true;
@@ -196,24 +195,28 @@ class Bug
       {
         this.sprite.vel.x = -score*1.1;
         this.sprite.scale.x = -1;
+        skitter.start();
       }
 
       else if(this.sprite.x < 5)
       {
         this.sprite.vel.x = score*1.1;
         this.sprite.scale.x = 1;
+        skitter.start();
       }
 
       else if(this.sprite.y < 5)
       {
         this.sprite.vel.y = score*1.1;
         this.sprite.scale.y = -1;
+        skitter.start();
       }
 
       else if(this.sprite.y > height - 5)
       {
         this.sprite.vel.y = -score*1.1;
         this.sprite.scale.y = 1;
+        skitter.start();
       }
     }
   }
